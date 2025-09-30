761e18434a0a6e288fb86a6334a12d25ec70588f
raviraj879214 <ravirajalphainfo@gmail.com>
2025-09-30 16:15:25 +0530

    gfgfg

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
   app.enableCors({
     origin: ['https://health-tech-ui.vercel.app'], // your frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   });
 
